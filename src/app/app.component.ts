13147c5b178536eb619ae3fd30f9b4bed5cae50a
Mac <mac@Macs-MacBook-Pro.local>
2023-06-01 23:58:02 +0100

    initial commits for gadgetstore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { SelectItemGroup } from 'primeng/api';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { MenuItem } from 'primeng/api';
 
+interface City {
+  name: string;
+  code: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  cities!: City[];
+  selectedCity!: any;
+  cityGroup!: FormGroup;
+  items: MenuItem[];
+
+  constructor(private fb: FormBuilder) {
+    this.items = [
+      {
+        label: 'Home',
+        routerLink: '/home',
+      },
+      {
+        label: 'About',
+        routerLink: '/about',
+      },
+      {
+        label: 'Contact',
+        routerLink: '/contact',
+      },
+    ];
+  }
+
   title = 'ngprimeApp';
+  products!: any[];
+  images: string[] = [
+    'https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&fit=clip',
+    'https://media.gettyimages.com/id/157482029/photo/stack-of-books.jpg?s=612x612&w=gi&k=20&c=_Yaofm8sZLZkKs1eMkv-zhk8K4k5u0g0fJuQrReWfdQ=',
+    'https://images.unsplash.com/photo-1589998059171-988d887df646?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8OHx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80',
+    // Add more image paths as needed
+  ];
+
+  product = [
+    {
+      name: 'iPhone x',
+      inventoryStatus: 'INSTOCK',
+      price: 5000,
+    },
+  ];
+  responsiveOptions: any[] = [
+    {
+      breakpoint: '1024px',
+      numVisible: 3,
+      numScroll: 3,
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 2,
+      numScroll: 2,
+    },
+    {
+      breakpoint: '560px',
+      numVisible: 1,
+      numScroll: 1,
+    },
+  ];
+
+  // responsiveOptions!: any[];
+  ngOnInit() {}
+
+  getSeverity(status: string) {
+    switch (status) {
+      case 'INSTOCK':
+        return 'success';
+      case 'LOWSTOCK':
+        return 'warning';
+      case 'OUTOFSTOCK':
+        return 'danger';
+    }
+    return;
+  }
+
+  visible!: boolean;
+
+  showDialog() {
+    this.visible = true;
+    console.log('clicked');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { TabViewModule } from 'primeng/tabview';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselModule } from 'primeng/carousel';
+import { ToolbarModule } from 'primeng/toolbar';
+import { GalleriaModule } from 'primeng/galleria';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { AccordionModule } from 'primeng/accordion';
+import { MenubarModule } from 'primeng/menubar';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabViewModule,
+    ScrollPanelModule,
+    CarouselModule,
+    ToolbarModule,
+    GalleriaModule,
+    ButtonModule,
+    DialogModule,
+    BrowserAnimationsModule,
+    DropdownModule,
+    ReactiveFormsModule,
+    FormsModule,
+    InputTextModule,
+    AccordionModule,
+    MenubarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
